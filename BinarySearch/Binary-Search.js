e9da08e5fab5b825130918277ec3b347fa17e589
David Agasiev <62882376+Davidagasiev@users.noreply.github.com>
2020-06-28 02:05:10 +0400

    BinarySearch

--- BinarySearch/Binary-Search.js
+++ BinarySearch/Binary-Search.js
@@ -1,7 +1,7 @@
 let binarySearch = (list, target) => {
 	let left   = 0,
-		right  = list.length - 1,
-		middle = Math.floor((left + right) / 2);
+	    right  = list.length - 1,
+	    middle = Math.floor((left + right) / 2);
 	while (list[middle] !== target && left < right) {
 	 	target < list[middle] ? right = --middle : left = ++middle;
 	}
