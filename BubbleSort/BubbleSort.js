a08619a0ed9aa933bee65cec143dde8347468168
David Agasiev <davidagasiev2005@gmail.com>
2020-06-27 21:38:15 +0400

    BubbleSort

---
+++ BubbleSort/BubbleSort.js
@@ -0,0 +1,12 @@
+function bubbleSort(arr){
+  for(let i = arr.length; i > 0; i--){
+    for(let j = 0; j < i - 1; j++){
+      if(arr[j] > arr[j+1]){
+        let temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;         
+      }
+    }
+  }
+  return arr;
+}
