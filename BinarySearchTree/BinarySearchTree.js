8a93f6dc056451c568e6e22010181c2d0a0de5a3
David <davidagasiev2005@gmail.com>
2020-07-11 16:06:36 +0400

    BinarySearchTree"

--- BinarySearchTree/BinarySearchTree.js
+++ BinarySearchTree/BinarySearchTree.js
@@ -11,12 +11,12 @@ class BinarySearchTree {
         this.root = null;
     }
     insert(value){
-        let newNode = new Node(value);
+        var newNode = new Node(value);
         if(this.root === null){
             this.root = newNode;
             return this;
         }
-        let current = this.root;
+        var current = this.root;
         while(true){
             if(value === current.value) return undefined;
             if(value < current.value){
@@ -34,9 +34,25 @@ class BinarySearchTree {
             }
         }
     }
+    find(value){
+        if(this.root === null) return false;
+        var current = this.root,
+            found = false;
+        while(current && !found){
+            if(value < current.value){
+                current = current.left;
+            } else if(value > current.value){
+                current = current.right;
+            } else {
+                found = true;
+            }
+        }
+        if(!found) return undefined;
+        return current;
+    }
     contains(value){
         if(this.root === null) return false;
-        let current = this.root,
+        var current = this.root,
             found = false;
         while(current && !found){
             if(value < current.value){
@@ -49,4 +65,48 @@ class BinarySearchTree {
         }
         return false;
     }
-}
+    BFS(){
+        var node = this.root,
+            data = [],
+            queue = [];
+        queue.push(node);
+
+        while(queue.length){
+           node = queue.shift();
+           data.push(node.value);
+           if(node.left) queue.push(node.left);
+           if(node.right) queue.push(node.right);
+        }
+        return data;
+    }
+    DFSPreOrder(){
+        var data = [];
+        function traverse(node){
+            data.push(node.value);
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+        }
+        traverse(this.root);
+        return data;
+    }
+    DFSPostOrder(){
+        var data = [];
+        function traverse(node){
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+            data.push(node.value);
+        }
+        traverse(this.root);
+        return data;
+    }
+    DFSInOrder(){
+        var data = [];
+        function traverse(node){
+            if(node.left) traverse(node.left);
+            data.push(node.value);
+            if(node.right) traverse(node.right);
+        }
+        traverse(this.root);
+        return data;
+    }
+}  
