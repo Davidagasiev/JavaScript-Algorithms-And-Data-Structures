798c3c4a00858b61fcfa0309e699f59c717ec7ad
David <davidagasiev2005@gmail.com>
2020-07-13 21:15:59 +0400

    BinarySearchTree

--- BinarySearchTree/BinarySearchTree.js
+++ BinarySearchTree/BinarySearchTree.js
@@ -34,22 +34,6 @@ class BinarySearchTree {
             }
         }
     }
-    find(value){
-        if(this.root === null) return false;
-        var current = this.root,
-            found = false;
-        while(current && !found){
-            if(value < current.value){
-                current = current.left;
-            } else if(value > current.value){
-                current = current.right;
-            } else {
-                found = true;
-            }
-        }
-        if(!found) return undefined;
-        return current;
-    }
     contains(value){
         if(this.root === null) return false;
         var current = this.root,
